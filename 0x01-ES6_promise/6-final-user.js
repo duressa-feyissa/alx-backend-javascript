41d6c91001c89ca6c6e29a9b6097a8d96fd9c1b9
duressa-feyissa <duresafeyisa2022@gmail.com>
2023-04-21 17:41:46 +0300

    alx

--- 0x01-ES6_promise/6-final-user.js
+++
@@ -1,7 +0,0 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-export default handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled
-
-}
